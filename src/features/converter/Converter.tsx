89a4668ac827e90f247b0a054863b7c21c00d24e
mdrbohlav <m.drbohlav1@gmail.com>
2024-02-04 22:58:49 +0100

    extract SVGs to separate components

--- src/features/converter/Converter.tsx
+++ src/features/converter/Converter.tsx
@@ -7,6 +7,7 @@ import Box from '../../ui/Box';
 import Button from '../../ui/Button';
 import ButtonIcon from '../../ui/ButtonIcon';
 import Error from '../../ui/Error';
+import RemoveIcon from '../../ui/RemoveIcon';
 import Separator from '../../ui/Separator';
 import Spacer from '../../ui/Spacer';
 import Spinner from '../../ui/Spinner';
@@ -79,12 +80,7 @@ function Converter() {
               >
                 {foreignCurrenciesIds.length > 1 && (
                   <ButtonIcon onClick={() => handleRemoveForeignCurrency(id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                      <path
-                        d="M480-424 284-228q-11 11-28 11t-28-11q-11-11-11-28t11-28l196-196-196-196q-11-11-11-28t11-28q11-11 28-11t28 11l196 196 196-196q11-11 28-11t28 11q11 11 11 28t-11 28L536-480l196 196q11 11 11 28t-11 28q-11 11-28 11t-28-11L480-424Z"
-                        fill="currentColor"
-                      />
-                    </svg>
+                    <RemoveIcon />
                   </ButtonIcon>
                 )}
               </ForeignCurrency>
--- src/ui/ThemeSwitch.tsx
+++ src/ui/ThemeSwitch.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import { brandColors, secondaryColors } from '../styles/GlobalStyles';
 import ButtonIcon from './ButtonIcon';
+import MoonIcon from './MoonIcon';
+import SunIcon from './SunIcon';
 
 function ThemeSwitch() {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(window.matchMedia('(prefers-color-scheme: dark)').matches);
@@ -14,32 +15,7 @@ function ThemeSwitch() {
     document.documentElement.classList.toggle('dark', isDarkMode);
   }, [isDarkMode]);
 
-  return (
-    <ButtonIcon onClick={handleToggle}>
-      {isDarkMode ? (
-        <svg viewBox="0 0 24 24" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-          <path
-            d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"
-            fill={brandColors[600]}
-          />
-          <path
-            d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
-            stroke={brandColors[600]}
-          />
-        </svg>
-      ) : (
-        <svg viewBox="0 0 24 24">
-          <path
-            d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            fill={secondaryColors[600]}
-          />
-        </svg>
-      )}
-    </ButtonIcon>
-  );
+  return <ButtonIcon onClick={handleToggle}>{isDarkMode ? <SunIcon /> : <MoonIcon />}</ButtonIcon>;
 }
 
 export default ThemeSwitch;
